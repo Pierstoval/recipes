7f54b6b8bc51cdae1469e4afeedb6c34f2bc3a0c
Alex Rock <pierstoval@gmail.com>
2018-09-25 11:24:23 +0200

    Update index.php

--- symfony/framework-bundle/4.2/public/index.php
+++ symfony/framework-bundle/4.2/public/index.php
@@ -2,21 +2,19 @@
 
 use App\Kernel;
 use Symfony\Component\Debug\Debug;
-use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Env\Env;
+use Symfony\Component\Env\EnvFile;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
 // The check is to ensure we don't use .env if APP_ENV is defined
-if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
-    if (!class_exists(Dotenv::class)) {
-        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
-    }
-    (new Dotenv())->load(__DIR__.'/../.env');
+if (!Env::has('APP_ENV')) {
+    (new EnvFile())->load(__DIR__.'/../.env');
 }
 
-$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
-$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
+$env = Env::get('APP_ENV') ?: 'dev';
+$debug = (bool) Env::get('APP_DEBUG') ?: ('prod' !== $env));
 
 if ($debug) {
     umask(0000);
@@ -24,11 +22,11 @@ if ($debug) {
     Debug::enable();
 }
 
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+if ($trustedProxies = Env::get('TRUSTED_PROXIES') ?: false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
 
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+if ($trustedHosts = Env::get('TRUSTED_HOSTS') ?: false) {
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
